59d7724dc7613993f4cc331c5d17fd0fe178e1e0
Mario Diana <software@mariodiana.com>
2021-07-10 20:26:20 -0400

    Add configuration and code for demo
    Because:
    
      * React Native packages need to be installed.
      * The demo code needs to replace the template's default.
    
    The first two pages of the following tutorial lists all of the packages
    required to run the demo:
    
        https://reactnavigation.org/docs/getting-started

--- App.tsx
+++ App.tsx
@@ -1,115 +1,87 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import * as React from 'react';
 
- import React from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
+import { StyleSheet, SafeAreaView, Switch, Text, Button } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
+import {SwitchContextProvider, useSwitchState} from './switch-context';
 
- const Section: React.FC<{
-   title: string;
- }> = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
+const HomeScreen = () => {
+  const switchState = useSwitchState();
 
- const App = () => {
-   const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <SafeAreaView>
+      <Switch
+        style={styles.switch}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={switchState.toggle}
+        value={switchState.isOn}
+      />
+    </SafeAreaView>
+  );
+};
 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
+const AwayScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.centeredText}>Hello, World!</Text>
+    </SafeAreaView>
+  );
+};
 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
+const LoginButton = (): JSX.Element => {
+  const navigation = useNavigation();
+  const switchState = useSwitchState();
 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
+  const handleClick = () => {
+    navigation.navigate('Away');
+  };
 
- export default App;
+  return (
+    <Button title="Login"
+            color="white"
+            disabled={!switchState.isOn}
+            onPress={handleClick} />
+  );
+};
+
+const MainStack = createStackNavigator();
+
+const MainStackScreen = (): JSX.Element => {
+  return (
+    <MainStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: 'red',
+        }, 
+        headerTintColor: 'white',
+      }}
+    >
+      <MainStack.Screen name="Home" component={HomeScreen} options={{title: "Home", headerRight: LoginButton}} />
+      <MainStack.Screen name="Away" component={AwayScreen} options={{title: "Hello"}} />
+    </MainStack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <SwitchContextProvider>
+      <NavigationContainer>
+        {MainStackScreen()}
+      </NavigationContainer>
+    </SwitchContextProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  switch: {
+    margin: 20,
+  },
+  centeredText: {
+    textAlign: 'center',
+    margin: 20,
+    fontSize: 20,
+  },
+});
+
+export default App;
---
+++ switch-context.tsx
@@ -0,0 +1,34 @@
+// https://kentcdodds.com/blog/application-state-management-with-react
+
+import React, { SetStateAction } from 'react';
+
+type SwitchStateTuple = [boolean, React.Dispatch<SetStateAction<boolean>>];
+type SwitchState = { isOn: boolean, setOn: React.Dispatch<SetStateAction<boolean>>, toggle: () => void };
+
+const SwitchContext = React.createContext<SwitchStateTuple>(null!);
+
+const useSwitchState = (): SwitchState => {
+    const context = React.useContext(SwitchContext);
+
+    if (!context) {
+        throw new Error(`useSwitchState must be used within a SwitchProvider.`);
+    }
+
+    const [isOn, setOn] = context;
+
+    const toggle = () => {
+        return setOn((b: boolean) => !b);
+    };
+
+    // We can now return this SwitchState object which allows us to access all this. 
+    return { isOn, setOn, toggle };
+};
+
+const SwitchContextProvider = (props: object) => {
+    const [isOn, setOn] = React.useState(false);
+    const [value, setValue] = React.useMemo(() => [isOn, setOn], [isOn]);
+
+    return (<SwitchContext.Provider value={[value, setValue]} {...props} />);
+};
+
+export { SwitchContextProvider, useSwitchState };
